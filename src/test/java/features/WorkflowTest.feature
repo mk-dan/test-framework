867267fa4c9c24f98a90b39843a9e5051299fcc1
Mark Daniels <markdaniels@live.co.uk>
2017-11-30 21:12:04 +0000

    feature update and addition of page objects

--- src/test/java/features/WorkflowTest.feature
+++ src/test/java/features/WorkflowTest.feature
@@ -1,13 +1,6 @@
-# new feature
-# Tags: optional
-    
-Feature:
-  As a user
-  I want to be able to read the news
-  So that i can stay informed
+Feature: Visit Guardian website in order to open main news article
 
-
-Scenario: Go to the guardian website
+  Scenario: Open main article in the Guardian website
     Given I am on the guardian website
     When I select the main article
     Then the article is opened
---
+++ src/test/java/pageobjects/UkPage.java
@@ -0,0 +1,32 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.openqa.selenium.support.ui.Select;
+
+public class UkPage extends BaseClass {
+
+	public UkPage(WebDriver driver)
+	{
+		super(driver);
+	}
+	
+	@FindBy(how= How.CSS, using=".selector,.hover")
+	public static WebElement div_heading;
+
+
+	@FindBy(how= How.ID, using="id_contact")
+	public static Select subject_heading;
+
+
+	@FindBy(how= How.CLASS_NAME, using="fc-item_content")
+	public static WebElement error_message;
+
+
+	
+
+
+
+}
